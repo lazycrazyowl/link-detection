62d0f53cbb3c646569c35da7a855abeaaa7a9945
Mog <mog@crydee.eu>
2014-01-17 17:32:38 +0100

    correct test suite after modification of the lexicalchain object

--- src/test/java/linkInterMessageDetector/datamodel/TestLexicalChain.java
+++ src/test/java/linkInterMessageDetector/datamodel/TestLexicalChain.java
@@ -6,26 +6,28 @@ import org.junit.Test;
 
 public class TestLexicalChain {
 
-	@Test
-	public void test() {
-		LexicalChain lc1 = new LexicalChain();
-		lc1.addItem("père");
-		lc1.addItem("noël");
-		lc1.addItem("santa");
-		lc1.addItem("sapin");
-		lc1.addItem("neige");
-		lc1.addItem("vinasse");
-		
-		LexicalChain lc2 = new LexicalChain();
-		lc2.addItem("père");
-		lc2.addItem("fouettard");
-		lc2.addItem("martinet");
-		lc2.addItem("enfant");
-		lc2.addItem("vinasse");
-		
-		System.out.printf("Comparing %s with %s gives %f which stands for %s \n",lc1, lc2, lc1.compare(lc2), lc1.isSimilar(lc2) ? "similar" : "notSimilar");
-		
-		
-	}
+    @Test
+    public void test() {
+        LexicalChain lc1 = new LexicalChain();
+        lc1.add("père");
+        lc1.add("noël");
+        lc1.add("santa");
+        lc1.add("sapin");
+        lc1.add("neige");
+        lc1.add("vinasse");
 
+        LexicalChain lc2 = new LexicalChain();
+        lc2.add("père");
+        lc2.add("fouettard");
+        lc2.add("martinet");
+        lc2.add("enfant");
+        lc2.add("vinasse");
+
+        System.out.printf(
+                "Comparing %s with %s gives %f which stands for %s \n",
+                lc1,
+                lc2,
+                lc1.compare(lc2),
+                lc1.isSimilar(lc2) ? "similar" : "notSimilar");
+    }
 }
